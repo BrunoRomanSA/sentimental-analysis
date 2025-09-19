1d28bd1b4b2ffafd9350ed08692cee1a5bc35d22
Bruno <brunoroman05@gmail.com>
2025-09-18 22:47:23 -0300

    coment ecr

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,6 @@ output "lambda_function_name" {
   value = aws_lambda_function.imdb_lambda.function_name
 }
 
-output "ecr_repo_url" {
-  value = aws_ecr_repository.lambda_repo.repository_url
-}
+# output "ecr_repo_url" {
+#   value = aws_ecr_repository.lambda_repo.repository_url
+# }
