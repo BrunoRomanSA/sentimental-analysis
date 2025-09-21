2299318c19bf3365305c4647d9820001d2b3dfd8
Bruno <brunoroman05@gmail.com>
2025-09-21 16:53:43 -0300

    return obrigatorio

--- infra/apigateway.tf
+++ infra/apigateway.tf
@@ -46,10 +46,10 @@ resource "aws_api_gateway_integration" "reviews_options_integration" {
   type        = "MOCK" # Uma integração MOCK é usada para o CORS
 }
 
-resource "aws_api_gateway_method_response" "reviews_options_response" {
+resource "aws_api_gateway_method_response" "reviews_post_response" {
   rest_api_id = aws_api_gateway_rest_api.sentiment_api.id
   resource_id = aws_api_gateway_resource.reviews_resource.id
-  http_method = aws_api_gateway_method.reviews_options_method.http_method
+  http_method = aws_api_gateway_method.reviews_post_method.http_method
   status_code = "200"
 
   response_parameters = {
@@ -59,15 +59,11 @@ resource "aws_api_gateway_method_response" "reviews_options_response" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "reviews_options_integration_response" {
+resource "aws_api_gateway_integration_response" "reviews_post_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.sentiment_api.id
   resource_id = aws_api_gateway_resource.reviews_resource.id
-  http_method = aws_api_gateway_method.reviews_options_method.http_method
-  status_code = aws_api_gateway_method_response.reviews_options_response.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
+  http_method = aws_api_gateway_method.reviews_post_method.http_method
+  status_code = aws_api_gateway_method_response.reviews_post_response.status_code
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
