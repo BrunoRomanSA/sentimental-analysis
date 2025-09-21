6058885a34e4b8b48279827470662e822da9d2f4
Bruno <brunoroman05@gmail.com>
2025-09-21 18:15:41 -0300

    ajuste options config

--- infra/apigateway.tf
+++ infra/apigateway.tf
@@ -16,7 +16,7 @@ resource "aws_api_gateway_method" "reviews_post_method" {
   rest_api_id   = aws_api_gateway_rest_api.sentiment_api.id
   resource_id   = aws_api_gateway_resource.reviews_resource.id
   http_method   = "POST"
-  authorization = "NONE" # Nenhuma autorização, já que a API será pública
+  authorization = "NONE"
 }
 
 # --- 4. Define a Integração entre o Método POST e a Função Lambda ---
@@ -25,11 +25,12 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   resource_id             = aws_api_gateway_resource.reviews_resource.id
   http_method             = aws_api_gateway_method.reviews_post_method.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY" # Integração direta com a Lambda (tipo proxy)
+  type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:503561450616:function:imdb-sentiment-lambda/invocations"
 }
 
 # --- 5. Habilita o CORS ---
+# OPTIONS method
 resource "aws_api_gateway_method" "reviews_options_method" {
   rest_api_id   = aws_api_gateway_rest_api.sentiment_api.id
   resource_id   = aws_api_gateway_resource.reviews_resource.id
@@ -42,8 +43,12 @@ resource "aws_api_gateway_integration" "reviews_options_integration" {
   resource_id = aws_api_gateway_resource.reviews_resource.id
   http_method = aws_api_gateway_method.reviews_options_method.http_method
   type        = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\":200}"
+  }
 }
 
+# Resposta do POST
 resource "aws_api_gateway_method_response" "reviews_post_response" {
   rest_api_id = aws_api_gateway_rest_api.sentiment_api.id
   resource_id = aws_api_gateway_resource.reviews_resource.id
@@ -70,6 +75,37 @@ resource "aws_api_gateway_integration_response" "reviews_post_integration_respon
   }
 }
 
+# Resposta do OPTIONS (CORS)
+resource "aws_api_gateway_method_response" "reviews_options_response" {
+  rest_api_id = aws_api_gateway_rest_api.sentiment_api.id
+  resource_id = aws_api_gateway_resource.reviews_resource.id
+  http_method = aws_api_gateway_method.reviews_options_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "reviews_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.sentiment_api.id
+  resource_id = aws_api_gateway_resource.reviews_resource.id
+  http_method = aws_api_gateway_method.reviews_options_method.http_method
+  status_code = aws_api_gateway_method_response.reviews_options_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
 # --- 6. Define o "Deploy" e o "Stage" da API ---
 resource "aws_api_gateway_deployment" "reviews_deployment" {
   rest_api_id = aws_api_gateway_rest_api.sentiment_api.id
@@ -78,7 +114,9 @@ resource "aws_api_gateway_deployment" "reviews_deployment" {
     aws_api_gateway_method.reviews_post_method,
     aws_api_gateway_integration.lambda_integration,
     aws_api_gateway_method_response.reviews_post_response,
-    aws_api_gateway_integration_response.reviews_post_integration_response
+    aws_api_gateway_integration_response.reviews_post_integration_response,
+    aws_api_gateway_method_response.reviews_options_response,
+    aws_api_gateway_integration_response.reviews_options_integration_response
   ]
 
   triggers = {
@@ -86,7 +124,9 @@ resource "aws_api_gateway_deployment" "reviews_deployment" {
       aws_api_gateway_method.reviews_post_method.id,
       aws_api_gateway_integration.lambda_integration.id,
       aws_api_gateway_method_response.reviews_post_response.id,
-      aws_api_gateway_integration_response.reviews_post_integration_response.id
+      aws_api_gateway_integration_response.reviews_post_integration_response.id,
+      aws_api_gateway_method_response.reviews_options_response.id,
+      aws_api_gateway_integration_response.reviews_options_integration_response.id
     ]))
   }
 }
