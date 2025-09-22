9c4092ec37c932b8ff5b38cf1bf6e914c6757d60
Bruno <brunoroman05@gmail.com>
2025-09-22 15:08:59 -0300

    retirar o depends on

--- infra/cloudwatch.tf
+++ infra/cloudwatch.tf
@@ -51,7 +51,6 @@ resource "aws_cloudwatch_metric_alarm" "accuracy_alarm" {
 
 resource "aws_sns_topic" "ml_model_alerts" {
   name = "ml-model-accuracy-alerts"
-  depends_on = [aws_cloudwatch_metric_alarm.accuracy_alarm]
 }
 
 resource "aws_sns_topic_subscription" "email_alert" {
